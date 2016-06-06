4881bf5fd02ec2daa38001e57a12848c3720f09c
titomilton <miltonsiqueira@gmail.com>
2016-06-06 11:37:32 +0000

    Http Collect

--- program.js
+++ program.js
@@ -1,7 +1,14 @@
 var http = require("http");
 
 http.get(process.argv[2], function(response){
+        var msg = "";
         response.setEncoding("utf8");
         response.on("error", console.error);
-        response.on("data", console.log);
+        response.on("data", function(data){
+            msg += data;
+        });
+        response.on("end", function(){
+            console.log(msg.length);
+            console.log(msg);
+        });
     }).on("error", console.error);
