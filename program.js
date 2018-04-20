14791392f8aa7c3597b145d33eec6de8306e7503
titomilton <miltonsiqueira@gmail.com>
2018-04-20 20:13:43 +0000

    juggling async

--- program.js
+++ program.js
@@ -1,14 +1,22 @@
 var http = require("http");
+//console.log("process.argv " + process.argv);
+var i = 2;
 
-http.get(process.argv[2], function(response){
-        var msg = "";
-        response.setEncoding("utf8");
-        response.on("error", console.error);
-        response.on("data", function(data){
-            msg += data;
-        });
-        response.on("end", function(){
-            console.log(msg.length);
-            console.log(msg);
-        });
-    }).on("error", console.error);
+
+var callback = function (response) {
+    var msg = "";
+    response.setEncoding("utf8");
+    response.on("error", console.error);
+    response.on("data", function(data){
+        msg += data;
+    });
+    response.on("end", function(){
+        console.log(msg);
+        if (i < 5) {
+            i++;
+            http.get(process.argv[i], callback).on("error", console.error);
+        }
+    });
+};
+
+http.get(process.argv[i], callback).on("error", console.error);
